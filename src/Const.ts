ae67d0fcbf4e0156748d5dd2e29870bf0a28efc9
Alejandro <alexulloafu@gmail.com>
2021-12-29 17:14:57 -0500

    updated consts

--- src/Const.ts
+++ src/Const.ts
@@ -3,13 +3,13 @@ dotenv.config();
 export default{
     BOT_API_TOKEN: process.env.NODE_ENV  == "dev" 
     ? "1906027291:AAFHscvCR_28huhnEfvQWRKQgefY89ItUTs" 
-    : "5040414565:AAFiPv17XH6rsYAbMtcppkt3WDrrHw4IzwE",
+    : process.env.API_BOT_TOKEN,
 
     BOT_DB_URL: process.env.NODE_ENV  == "dev" 
     ? 'mongodb://127.0.0.1/ScholarBotDB'
-    : 'mongodb://127.0.0.1/ScholarBotDB',
+    : process.env.URL_MONGO_ATLAS,
 
     BOT_USERNAME: process.env.NODE_ENV == "dev"
     ? 'scholar_scholarship_bot'
-    : "axie_scholarships_launcher_bot"
+    : process.env.BOT_USERNAME
 }
